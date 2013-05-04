3fa1c6f0856c220dbc3884f40b92a8193fe4290f
murphyrandle <murphyspublic@gmail.com>
2013-05-04 12:28:03 -0600

    Refactored

--- server.py
+++ server.py
@@ -4,59 +4,38 @@ import random
 from collections import OrderedDict
 
 urls = (
-    '/entry/(.+)/(.+)', 'entry',
-    '/entry/(.+)', 'entry',
-    '/entry/', 'entry',
-    '/entry', 'entry',
-    '/list/(\d*)', 'list',
-    '/list', 'list'
+    '/entry/?([^/]*)/?([^/]*)', 'entry',
+    '/list/?(\d*)', 'list',
 )
 app = web.application(urls, globals())
 model = OrderedDict()
 
 
 class entry:
-    def GET(self, key=None):
-        # Test if no key was specified, return a random entry.
-        if not key:
-            if not model.keys():
-                return app.notfound()
+    def GET(self, key, _):
+        if not model.keys() or (key and not model.get(key)):
+            return app.notfound()
+        elif key:
+            return model.get(key)
+        else:
             randomkey = random.choice(model.keys())
             return "{key} -> {value}".format(key=randomkey,
                                              value=model[randomkey])
 
-        # A key was specified, look for it in the model.
-        response = model.get(key)
-        if response:
-            return response
-        else:
-            return app.notfound()
-
     def POST(self, key=None, value=None):
-        if not self.validate(key, value):
-            raise web.HTTPError('400 BAD REQUEST')
-
-        model[key] = value
-
-    def validate(self, *args):
-        for arg in args:
+        for arg in (key, value):
             if not arg or len(arg) >= 128:
-                return False
-        return True
+                raise web.HTTPError('400 BAD REQUEST')
+        model[key] = value
 
 
 class list:
     def GET(self, page=0):
         page = 0 if not page else page
-        retstrings = []
-        for i in xrange(0, 4):
-            index = int(page) * 4 + i
-            if index < len(model.items()):
-                item = model.items()[index]
-                retstrings.append("{key} -> {value}".format(
-                    key=item[0], value=item[1]))
-        return "\n".join(retstrings)
-
+        begindex = int(page)*4
+        items = model.items()[begindex:begindex+4]
+        return "\n".join(["{key} -> {value}".format(key=item[0], value=item[1])
+                         for item in items])
 
 if __name__ == "__main__":
     app.run()
